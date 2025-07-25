a453e6ea80f6f84233e7d618c6a5d6ede0ffd982
lazim-mv <lazimlatheef@gmail.com>
2025-07-25 22:50:29 +0530

    projectprocess

--- src/app/components/testimonials/content.js
+++ src/app/components/testimonials/content.js
@@ -40,36 +40,36 @@ export const testimonialsData = [
       "Our old UI felt a bit outdated, so we brought Lazim in to improve it. He brought a fresh look to the site and paid attention to the little things like fonts and buttons that really made a difference. Everything felt pretty seamless from start to finish.",
     image: "/avatars/aisha.jpg",
   },
-  {
-    name: "Mohammed Al Saadi",
-    initial: "MAS",
-    company: "Brightline",
-    testimonial:
-      "We wanted a website that could truly reflect the spirit of our school not just information, but something that captures our students’ energy and achievements. Lazim understood that right away. He designed a platform where we can share moments, highlight student work, and keep parents informed. The site feels personal, vibrant, and easy for our team to manage. It’s made a real difference in how we present ourselves to the community.",
-    image: "/avatars/mohammed.jpg",
-  },
-  {
-    name: "Adil Mohammed",
-    initial: "AM",
-    company: "Igreenie",
-    testimonial:
-      "Lazim gave our website a much-needed upgrade. It’s now mobile-friendly, loads faster, and fits our brand better. He also paid attention to performance and SEO, which we hadn’t really thought about before. We’re already seeing some positive results.",
-    image: "/avatars/julia.jpg",
-  },
-  {
-    name: "Imran Qureshi",
-    initial: "IQ",
-    company: "Service Sphere",
-    testimonial:
-      "We needed a reliable mobile app to help connect service providers with customers, and Lazim helped us get there. He understood what we were trying to build and worked closely with us to make sure the design and flow made sense. The app feels smooth and easy to use, and we're happy with how it turned out. Would definitely consider working with him again in the future.",
-    image: "/avatars/imran.jpg",
-  },
-  {
-    name: "Rahul Deshmukh",
-    initial: "RD",
-    company: "Prime Wheels Hub",
-    testimonial:
-      "Working with Lazim made a big difference for our used car business. Before, managing everything inventory, service records, photos was scattered and time-consuming. Now, it’s much more organized, and updating listings is way easier. The dashboard isn't flashy, but it works really well and does what we need. He also made sure we understood how to use everything without needing help every time. It’s been a solid upgrade for us.",
-    image: "/avatars/rahul.jpg",
-  }
+  // {
+  //   name: "Mohammed Al Saadi",
+  //   initial: "MAS",
+  //   company: "Brightline",
+  //   testimonial:
+  //     "We wanted a website that could truly reflect the spirit of our school not just information, but something that captures our students’ energy and achievements. Lazim understood that right away. He designed a platform where we can share moments, highlight student work, and keep parents informed. The site feels personal, vibrant, and easy for our team to manage. It’s made a real difference in how we present ourselves to the community.",
+  //   image: "/avatars/mohammed.jpg",
+  // },
+  // {
+  //   name: "Adil Mohammed",
+  //   initial: "AM",
+  //   company: "Igreenie",
+  //   testimonial:
+  //     "Lazim gave our website a much-needed upgrade. It’s now mobile-friendly, loads faster, and fits our brand better. He also paid attention to performance and SEO, which we hadn’t really thought about before. We’re already seeing some positive results.",
+  //   image: "/avatars/julia.jpg",
+  // },
+  // {
+  //   name: "Imran Qureshi",
+  //   initial: "IQ",
+  //   company: "Service Sphere",
+  //   testimonial:
+  //     "We needed a reliable mobile app to help connect service providers with customers, and Lazim helped us get there. He understood what we were trying to build and worked closely with us to make sure the design and flow made sense. The app feels smooth and easy to use, and we're happy with how it turned out. Would definitely consider working with him again in the future.",
+  //   image: "/avatars/imran.jpg",
+  // },
+  // {
+  //   name: "Rahul Deshmukh",
+  //   initial: "RD",
+  //   company: "Prime Wheels Hub",
+  //   testimonial:
+  //     "Working with Lazim made a big difference for our used car business. Before, managing everything inventory, service records, photos was scattered and time-consuming. Now, it’s much more organized, and updating listings is way easier. The dashboard isn't flashy, but it works really well and does what we need. He also made sure we understood how to use everything without needing help every time. It’s been a solid upgrade for us.",
+  //   image: "/avatars/rahul.jpg",
+  // }
 ];
