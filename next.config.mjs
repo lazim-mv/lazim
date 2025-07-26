e3d9b7d3c5817296d241aaaf115b3f1ee299db49
lazim-mv <lazimlatheef@gmail.com>
2025-07-26 15:48:19 +0530

    emailjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/** Needed because __dirname is not available in ESM */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {},
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  }
 };
 
 export default nextConfig;
